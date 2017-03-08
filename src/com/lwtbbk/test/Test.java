3cd84aef55e4da1535b9c0b7429bff3ef00e4c93
lekoxnfx <lekoxnfx@lekoxnfxdeMacBook-Pro.local>
2017-03-08 16:50:01 +0800

    Initial commit

---
+++ src/com/lwtbbk/test/Test.java
@@ -0,0 +1,11 @@
+package com.lwtbbk.test;
+
+/**
+ * Created by lekoxnfx on 2017/3/8.
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("Test Success!");
+    }
+}
